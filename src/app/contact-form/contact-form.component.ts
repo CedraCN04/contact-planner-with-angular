777fd503d11eb0dd1784e27c7bc7cfa2067f2e17
GuillaumeCeccoli <guiceccoli@gmail.com>
2024-10-04 19:26:41 +0200

    ajout d'un toogle pour faire apparaitre / disparaitre le formulaire

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,3 +1,4 @@
+import { NgIf } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Contact } from '../models/contact';
@@ -5,16 +6,22 @@ import { Contact } from '../models/contact';
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent {
   @Output() contactCreated = new EventEmitter<Contact>();
   contact: Contact = new Contact(0, '', '', '', '');
+  showForm: boolean = false;
+
+  toogleForm() {
+    this.showForm = !this.showForm;
+  }
 
   onSubmit() {
     this.contactCreated.emit(this.contact);
     this.contact = new Contact(0, '', '', '', ''); // Réinitialiser le formulaire
+    this.showForm = false;
   }
 }
